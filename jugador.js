00b21ddf7c7abe6767151f12ae95a2749f55cc56
ProDerbyLive <lic.ramos04@gmail.com>
2026-06-20 14:44:31 -0600

    Update jugador.js

--- jugador.js
+++ jugador.js
@@ -226,7 +226,7 @@ function cargarVideoYoutube() {
     contenedor.innerHTML = `
         <div class="pelea-card">
 
-            <div style="position:relative; width:100%; padding-bottom:70%; height:0; overflow:hidden; border-radius:18px;">
+            <div style="position:relative; width:100%; padding-bottom:56.25%; height:0; overflow:hidden; border-radius:18px;">
                 <iframe
                     src="${embedUrl}"
                     style="position:absolute; top:0; left:0; width:100%; height:100%; border:0;"
